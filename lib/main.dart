2b98d18746de6f6f6d0045af61020c81a2673497
Aritroo <aritroo123chowdhury52@gmail.com>
2022-10-12 22:44:13 +0530

    2nd Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget
-{
-  BuildContext
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What is your favourite Colour?',
+      'What is your favourite Animal?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Personality Test Quiz'),
+        ),
+        body: Column(children: [
+          Text('The Question ???'),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answser 1'),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answser 2'),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answser 3'),
+          ),
+          ElevatedButton(
+            onPressed: null,
+            child: Text('Answser 4'),
+          ),
+        ]),
+      ),
+    );
+  }
 }
