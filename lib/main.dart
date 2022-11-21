d03615c032e039de4cc7d8ad21cea557eda83bcd
Aritroo <aritroo123chowdhury52@gmail.com>
2022-11-21 13:46:29 +0530

    SetState gets updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,53 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answserQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var question = [
+      'What is your name?',
+      'What is your fav colour?',
+      'Where are you form?'
+    ];
     return MaterialApp(
+      title: "Quiz_App",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dart Quiz"),
-        ),
-        body: Text("Sample Body"),
-      ),
+          appBar: AppBar(
+            title: Text("Dart Quiz"),
+          ),
+          body: Column(
+            children: [
+              Text(question[questionIndex]),
+              ElevatedButton(
+                onPressed: answserQuestion,
+                child: Text('Answser 1'),
+              ),
+              ElevatedButton(
+                  onPressed: answserQuestion, child: Text('Answser 2')),
+              ElevatedButton(
+                  onPressed: answserQuestion, child: Text('Answser 3')),
+            ],
+          )),
     );
   }
 }
